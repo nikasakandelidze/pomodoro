62f60f8be9b761e6e43dcfd51def3a5dadf98066
nikasakana <nikasakana@gmail.com>
2021-09-25 11:54:59 +0400

    added portals for modal popup message

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,17 +1,33 @@
+import { useRef } from 'react';
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react/cjs/react.development';
 import '../../styles/Modal.css';
 import CustomButton from '../UI/Button';
 
-const Modal = ({ handleClose, show, children }) => {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
+const modalRoot = document.getElementById('modal-placeholder')
 
-  return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
-        {children}
-        <CustomButton title={'Close'} onClickCallback={handleClose} />
-      </section>
-    </div>
-  );
+const Modal = ({ handleClose, show, children }) => {
+    const elementRef = useRef(null);
+    if(!elementRef.current){
+      elementRef.current = document.createElement('div')
+    }
+
+    useEffect(()=>{
+        modalRoot.appendChild(elementRef.current)
+        return ()=>modalRoot.removeChild(elementRef.current)  
+    }, [])
+
+    return createPortal(<PopupModal handleClose={handleClose} show={show}>{children}</PopupModal>, elementRef.current)
 };
+
+const PopupModal = ({handleClose, children, show}) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+  return (<div className={showHideClassName}>
+  <section className="modal-main">
+    {children}
+    <CustomButton title={'Close'} onClickCallback={handleClose} />
+  </section>
+</div>)
+}
 
 export default Modal;
